1fd660fac561b508c54aaf8b1e9efce705d1a05f
Anna <an.martynova@astondevs.ru>
2023-06-08 09:02:49 +0300

    add logs, javadoc

---
+++ src/main/java/com/example/registrationsystem/util/filter/LoggingFilter.java
@@ -0,0 +1,65 @@
+package com.example.registrationsystem.util.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.ContentCachingRequestWrapper;
+import org.springframework.web.util.ContentCachingResponseWrapper;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
+/**
+ * Filter base class that aims to guarantee a single execution per request dispatch, on any servlet container.
+ */
+@Slf4j
+public class LoggingFilter extends OncePerRequestFilter {
+
+    /**
+     * Same contract as for {@code doFilter}, but guaranteed to be
+     * just invoked once per request within a single request thread.
+     * See {@link #shouldNotFilterAsyncDispatch()} for details.
+     *
+     * <p>Provides HttpServletRequest and HttpServletResponse arguments instead of the
+     * default ServletRequest and ServletResponse ones.
+     *
+     * @param request {@link HttpServletRequest}
+     * @param response {@link HttpServletResponse}
+     * @param filterChain object provided by the servlet container to the developer giving a view into the invocation
+     *     chain of a filtered request for a resource
+     */
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        ContentCachingRequestWrapper requestWrapper = new ContentCachingRequestWrapper(request);
+        ContentCachingResponseWrapper responseWrapper = new ContentCachingResponseWrapper(response);
+
+        long startTime = System.currentTimeMillis();
+        MDC.put("clientId", request.getParameter("clientId"));
+        filterChain.doFilter(requestWrapper, responseWrapper);
+        long timeTaken = System.currentTimeMillis() - startTime;
+
+        String requestBody = getStringValue(requestWrapper.getContentAsByteArray(),
+                request.getCharacterEncoding());
+        String responseBody = getStringValue(responseWrapper.getContentAsByteArray(),
+                response.getCharacterEncoding());
+        log.info("FINISHED PROCESSING : METHOD={}; REQUESTURI={}; REQUEST PAYLOAD={}; RESPONSE CODE={}; RESPONSE={}; "
+                        + "TIME TAKEN={}",
+                request.getMethod(), request.getRequestURI(), requestBody, response.getStatus(), responseBody,
+                timeTaken);
+        responseWrapper.copyBodyToResponse();
+    }
+
+    private String getStringValue(byte[] contentAsByteArray, String characterEncoding) {
+        try {
+            return new String(contentAsByteArray, 0, contentAsByteArray.length, characterEncoding);
+        } catch (UnsupportedEncodingException e) {
+            log.error("The Character Encoding is not supported");
+        }
+        return "";
+    }
+}
